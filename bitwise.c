adf3d5288fd7bdedea0556026cbd3dcd396a20d6
manumr13 <manumr9482@gmail.com>
2023-09-16 13:08:35 +0530

    adding file

---
+++ bitwise.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int x = 5;
+    printf("%d",x<<1): // here the left shift can be used as multiplication by 2
+    int y = 10;
+    printf("%d",y>>1); //here the right shift can be use as divide by 2 // if y>>2 then divide by 4
+}
--- first_project.c
+++ first_project.c
@@ -1,4 +1,19 @@
 #include<stdio.h>
 int main(){
-    printf("%d","manu");
+    int a , b;
+    printf("Enter the number upto which you want to find the even and odd :");
+    scanf("%d",&a);
+    printf("enter the number that you want to find the multiplication :");
+    scanf("%d",&b);
+    for (int i; i<=a; i++){
+        if (i%2==0){
+            printf("even : %d\n",i);
+        }
+        else{
+            printf("odd : %d\n",i);
+        }
+    }
+    for(int j; j<=10 ;j++){
+        printf("Multiplication of the numbers : %d * %d = %d\n",b,j,b*j);
+    }
 }
